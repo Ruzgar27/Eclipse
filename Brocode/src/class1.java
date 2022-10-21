d759fe9c5b60ab7c3c63a0652f9f256ece51ddac
Rüzgar <Rzgar@DESKTOP-D7Q51J7>
2022-10-21 12:51:19 +0300

    first

---
+++ Brocode/src/class1.java
@@ -0,0 +1,20 @@
+
+public class class1 {
+
+	public static void main(String[] args) {
+		
+	 String x = "çay";
+	 String y = "kahve";
+	 String z = null;
+
+	 z = x ;
+	 x = y ; 
+	 y = z ;
+	 
+	 System.out.println("x: " + x);
+	 System.out.println("y: " + y);
+	 
+		
+	}
+
+}
