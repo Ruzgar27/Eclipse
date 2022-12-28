5310b8a2446bff2f95feb9db62ea7ce929777da6
Rüzgar <Rzgar@DESKTOP-D7Q51J7>
2022-12-28 19:17:40 +0300

    number guess

---
+++ Number Guessing Game/src/Numberguessingame.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Numberguessingame {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		int k = 25;
+
+		int r = random.nextInt(100)+1;		
+		
+		for(int i = 0 ; i < k ; i++) {
+
+			System.out.println("Please guess the number between 1 and 100 "+"You have "+(k-i)+" tries left.");
+			
+			int Guess = scanner.nextInt();
+			
+			if(Guess==r){
+				
+				System.out.println("Thats Correct! Congrats!");
+				
+			}else if(Guess<r&&i!=k-1) {
+				
+				System.out.println("Thats too low!");
+				
+			} else if(Guess>r&&i!=k-1) {
+				
+				System.out.println("Thats too high!");
+				
+			}else if(i==k) {
+				System.out.println("You have exhausted all your tries!");
+				
+				System.out.println("The number was : "+r);
+			}
+
+
+			
+		}
+		
+
+	}
+
+}
